3c69a24ba557a57efc68e6fa9cd39aab222eda3e
DLeaguer <immostlikely@hotmail.com>
2018-07-20 23:14:05 -1000

    competed redo

--- vault.js
+++ vault.js
@@ -1,4 +1,24 @@
 'use strict';
 module.exports = function() {
-  
+
+    const vault = {};
+    
+    function getValue(key) {
+        if (key in vault) {
+            return vault[key];
+        }
+        else {
+            return null;
+        }
+    }
+   
+    function setValue(key, value){
+        vault[key] = value;
+    }
+
+    return {
+        setValue:setValue,
+        getValue:getValue
+    }
+
 };
